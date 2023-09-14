9ccaf0ebaf22d10796a15e9f87f198dc251a04d6
Monika Palle <91798@LAPTOP-G17RQ28H>
2023-09-14 15:55:26 +0530

    Updates

--- app/src/test/java/QKART_TESTNG/pages/Login.java
+++ app/src/test/java/QKART_TESTNG/pages/Login.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 
